9bf87769e1d00655a0e6d28e51ef6d28fb007576
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-30 14:28:22 -0700

    Add second GET test for single user

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -55,4 +55,25 @@ describe('API Routes', () => {
     });
   });
 
+  describe('GET /api/v1/users/:id', () => {
+    it('should return a single user', (done) => {
+      chai.request(server)
+      .get('/api/v1/1')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body[0].should.have.property('name');
+        res.body[0].name.should.equal('Brian Gordon');
+        res.body[0].should.have.property('email');
+        res.body[0].email.should.equal('brian@lax.com');
+        res.body[0].should.have.property('bio');
+        res.body[0].bio.should.equal('This is a relatively short bio.');
+        res.body[0].should.have.property('super_user');
+        res.body[0].super_user.should.equal(false);
+        done();
+      });
+    });
+  });
+
 });
