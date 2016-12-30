3c3a31518fe6ab9f05bc8bd8b3e8a276d6c0ffdb
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-30 14:15:29 -0700

    Update queries.js with helper and getAll function

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const knex = require('./knex');
+
+function users() {
+  return knex('users');
+}
+
+// *** queries *** //
+
+function getAll() {
+  return users().select();
+}
+
+module.exports = {
+  getAll: getAll
+};
