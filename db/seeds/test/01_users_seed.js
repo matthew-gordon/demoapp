9e874c4a1163dedbebd67bef0bfcb9fcaf0064d0
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-30 16:49:19 -0700

    Refactor seeds

--- db/seeds/test/01_users_seed.js
+++ db/seeds/test/01_users_seed.js
@@ -1,33 +1,31 @@
 'use strict';
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('users').insert({
-          id: 1,
-          name: 'Matt Gordon',
-          email: 'matt@lax.com',
-          bio: 'This is a relatively short bio.',
-          super_user: true
-        }),
-        knex('users').insert({
-          id: 2,
-          name: 'Brian Gordon',
-          email: 'brian@lax.com',
-          bio: 'This is a relatively short bio.'
-        }),
-        knex('users').insert({
-          id: 3,
-          name: 'Josh Gordon',
-          email: 'josh@lax.com',
-          bio: 'This is a relatively short bio.'
-        }),
-      ])
-      .then(() => {
-        return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
+  return knex('users').del() // Deletes ALL existing entries
+    .then(function() { // Inserts seed entries one by one in series
+      return knex('users').insert({
+        id: 1,
+        name: 'Matt Gordon',
+        email: 'matt@lax.com',
+        bio: 'This is a relatively short bio.',
+        super_user: true
       });
+    }).then(function () {
+      return knex('users').insert({
+        id: 2,
+        name: 'Brian Gordon',
+        email: 'brian@lax.com',
+        bio: 'This is a relatively short bio.'
+      });
+    }).then(function () {
+      return knex('users').insert({
+        id: 3,
+        name: 'Josh Gordon',
+        email: 'josh@lax.com',
+        bio: 'This is a relatively short bio.'
+      });
+    })
+    .then(() => {
+      return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
     });
 };
