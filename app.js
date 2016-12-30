044ec5cb69d7cd6706b5eb5cc75c93db9a6f41d2
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-30 12:30:26 -0700

    Add app.js and configuration, add /test && /routes

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const routes = require('./routes/index');
+
+const app  = express();
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+app.use(bodyParser.json());
+app.user(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handlers
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send(`There was and error; message: ${err.message} error: ${err}`);
+  });
+}
+
+// prosuction error handlers
+// no stacktrace leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(`There was and error; message: ${err.message} error: ${{}}`);
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
